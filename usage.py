8e5135097e223ae009d16d1a836cd154c021b852
Prash <prash.somaiya@gmail.com>
2013-03-20 21:29:23 +0000

    Usage file

--- usage.py
+++ usage.py
@@ -1,4 +1,10 @@
 from PyCurrency import *
 
+# Instantiate the class
 convert = PyCurrency()
+
+# How much USD is 10 GBP? Don't round it, please!
+print convert.convert(10, 'GBP', 'USD')
+
+# How much USD do I need for 20 EUR? Oh, and round it :)
 print convert.amountTo(20, 'USD', 'EUR', True)
